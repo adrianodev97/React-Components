9f16b94c506ad8dbd47fd163ed83e4693ce34092
Adriano Junior <vivaadrianonascimento@gmail.com>
2022-08-28 14:27:14 -0300

    Dropdown Settings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
+import { useNavigate } from "react-router-dom"
+import Dropdown from "./Dropdown"
+
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
+    const menuItems = [
+        {text: "Alert foo", onClick: () => {alert("foo")}},
+        {text: "Home", onClick: () => {navigate("/Home")}}]
+
     return (
         <>
-            
+            <header className="header">
+                <div className="header-container">
+                    <div className="header-content">
+                        <h1 className="logo">LOGO</h1>
+                        <nav className="nav">
+                            <li><Dropdown menuItems={menuItems} alignment="left">Menu</Dropdown></li>
+                        </nav>
+                        <div className="header-search">
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </header>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
-import { useNavigate } from "react-router-dom"
-import Dropdown from "../components/Dropdown"
 
 const Home = () => {
 
-    const navigate = useNavigate()
-
-    const menuItems = [
-        {text: "Alert foo", onClick: () => {alert("foo")}},
-        {text: "Home", onClick: () => {navigate("/Home")}}]
-
     return (
         <>
-            <Dropdown menuItems={menuItems}>Menu</Dropdown>
+            <section className="home">
+                <h1>Home</h1>
+            </section>
         </>
     )
 }
