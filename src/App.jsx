3393d8cb9af71788537a295bbf6845ad2985e3e6
Adriano Nascimento <adrianojunior.dev97@gmail.com>
2022-08-29 09:08:17 -0300

    Edits Dropdown Component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
       </Routes>
       <Footer />
     </>
